a6715472c5886c397d71ad00acab47f3dd806e07
Zhe Zeng <zhezeng@ZhedeMacBook-Pro.local>
2017-04-27 19:01:56 +0800

    add mcts

---
+++ python/board.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*- 
+
+import copy
+
+from chess import *
+from element import *
+
+# 对应pieces
+class Board:
+    def __init__(self):
+        pass
+
+    def current_player(self, state):
+        # Takes the game state and returns the current player's
+        # number.
+        # 返回轮到谁下了
+        (last_player,pieces) = state
+        this_player = 'w' if last_player == 'b' else 'b'
+        return this_player
+
+    def next_state(self, state, play):
+        # Takes the game state, and the move to be applied.
+        # Returns the new game state.
+        (last_player,pieces) = state
+        new_pieces = copy.deepcopy(pieces)
+        this_player = 'w' if last_player == 'b' else 'b'
+        (solutions, flips) = next_possible_steps(pieces, this_player)
+        
+        if play in solutions:
+            # 下了solution这一步之后的pieces
+            put_piece(play, this_player, new_pieces)
+            return (this_player,new_pieces)
+        else:
+            pass
+
+    def legal_plays(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history, and returns the full list of moves that
+        # are legal plays for the current player.
+        (last_player,pieces) = state_history[-1]
+        this_player = 'w' if last_player == 'b' else 'b'
+        (solutions, flips) = next_possible_steps(pieces,this_player)
+        return solutions
+
+    def winner(self, state_history):
+        # Takes a sequence of game states representing the full
+        # game history.  If the game is now won, return the player
+        # number.  If the game is still ongoing, return zero.  If
+        # the game is tied, return a different distinct value, e.g. -1.
+        last_state = state_history[-1]
+        (player,pieces) =  last_state
+        next_player = 'w' if player == 'b' else 'b'
+        # 判断结束
+        (solutions,flips) = next_possible_steps(pieces, next_player)
+        if solutions:
+            return False
+        else:
+            (solutions,flips) = next_possible_steps(pieces, player)
+            if solutions:
+                return False
+            else:
+                # 双方没得下，游戏结束
+                black = 0
+                white = 0
+                for i in range(8):
+                    for j in range(8):
+                        if not pieces[i][j]==None:
+                            if pieces[i][j] == 'b':
+                                black += 1
+                            else:
+                                white += 1
+
+                if black>white:
+                    return 'b'
+                elif black < white:
+                    return 'w'
+                else:
+                    return 't'
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/mcts.py
@@ -0,0 +1,166 @@
+# -*- coding:utf-8 -*- 
+from __future__ import division
+
+from random import choice
+from math import log, sqrt
+import datetime
+import copy
+
+from board import *
+import chess
+import element
+
+
+def state_to_string(state):
+
+    (player, pieces) = state
+    s = ''
+    for i in range(8):
+        for j in range(8):
+            if not pieces[i][j] == None:
+                s += pieces[i][j].color
+            else:
+                s += 'n'
+
+    return s
+
+class MonteCarlo:
+    def __init__(self, board):
+        # Takes an instance of a Board and optionally some keyword
+        # arguments.  Initializes the list of game states and the
+        # statistics tables.
+
+        # state: 包含了player信息，棋盘信息
+
+        self.board = board
+        self.states = []
+        # 模拟的时间限制
+        self.calculation_time = datetime.timedelta(seconds=5)
+        # 模拟中的最大步数
+        self.max_moves = 64
+        # 记录某个中间状态对应的模拟数、模拟中赢的盘数的字典
+        self.plays = {}
+        self.wins = {}
+        # UCB1的参数，先从根号2开始，目前来看越小越好
+        self.C = 0.001
+
+    def update(self, state):
+        # Takes a game state, and appends it to the history.
+        
+        self.states.append(state)
+
+    def get_play(self):
+        # Causes the AI to calculate the best move from the
+        # current game state and return it.
+        # 而且已经做好判断轮到了AI
+        self.max_depth = 0
+        state = self.states[-1]
+        # player 是 'w' or 'b'
+
+        player = self.board.current_player(state)
+        legal = self.board.legal_plays(self.states[:])
+        # (player,pieces)
+        # Bail out early if there is no real choice to be made.
+        if not legal:
+            return None
+        if len(legal) == 1:
+            return legal[0]
+
+        games = 0
+        begin = datetime.datetime.utcnow()
+        while datetime.datetime.utcnow() - begin < self.calculation_time:
+            self.run_simulation()
+            games += 1
+        print '************************'
+        print games
+        #print '************************'
+        #print self.plays.values()
+        #print '************************'
+        #print self.wins.values()
+
+        moves_states = [(p, self.board.next_state(state, p)) for p in legal]
+    
+        # Pick the move with the highest percentage of wins.
+        # player是造成state S的玩家
+        percent_wins, move = max(
+            (self.wins.get((player, state_to_string(S)), 0) /
+             self.plays.get((player, state_to_string(S)), 1),
+             p)
+            for p, S in moves_states
+        )
+
+        return move
+
+
+
+
+    def run_simulation(self):
+        # A bit of an optimization here, so we have a local
+        # variable lookup instead of an attribute access each loop.
+        plays, wins = self.plays, self.wins
+
+        # Plays out a "random" game from the current position,
+        # then updates the statistics tables with the result.
+        
+        visited_states = set()
+        states_copy = copy.deepcopy(self.states[:]) 
+        state = states_copy[-1]
+        player = self.board.current_player(state)
+
+        expand = True
+        for t in xrange(self.max_moves):
+            # legal：从state出发的可行步
+            legal = self.board.legal_plays(states_copy)
+            #print legal
+
+            # 判断legal是否为空
+
+            moves_states = [(p, self.board.next_state(state, p)) for p in legal]
+            # 选择p，以及state更新为选择了p后的状态
+            if len(moves_states)!=0 and all([plays.get((player, state_to_string(S))) for p, S in moves_states]):
+                # If we have stats on all of the legal moves here, use them.
+                summation = sum([plays[(player, state_to_string(S))] for p, S in moves_states])
+                log_total = log(summation)
+                #log_total = log(
+                #    sum([plays[(player, state_to_string(S))] for p, S in moves_states]))
+                #  这里基数小的时候，讲下面/改成*
+                value, move, state = max(
+                    ((wins[(player, state_to_string(S))] / plays[(player, state_to_string(S))]) +
+                     self.C * sqrt(log_total / plays[(player, state_to_string(S))]), p, S)
+                    for p, S in moves_states
+                )
+            elif len(moves_states)!=0:
+                # Otherwise, just make an arbitrary decision.
+                move, state = choice(moves_states)
+            else:
+                # solution里面没有元素了
+                next_player = 'w' if player=='b' else 'b'
+                (temp_player,temp_pieces) = state
+                state = (next_player, temp_pieces)
+
+
+            states_copy.append(state)
+
+            # `player` here and below refers to the player
+            # who moved into that particular state.
+            if expand and (player, state_to_string(state)) not in self.plays:
+                expand = False
+                self.plays[(player, state_to_string(state))] = 0
+                self.wins[(player, state_to_string(state))] = 0
+
+            visited_states.add((player, state_to_string(state)))
+
+            player = self.board.current_player(state)
+            winner = self.board.winner(states_copy)
+
+            if winner:
+                break
+
+        # 更新经过路径的数据
+        for player, state in visited_states:
+            if (player, state) not in self.plays:
+                continue
+            self.plays[(player, state)] += 1
+            # winner：赢的人的名字
+            if player == winner:
+                self.wins[(player, state)] += 1
